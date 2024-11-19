1bbe0984307ff356f6295128e8ab5951d485ef2f
seeyou7 <5764@holbertonstudents.com>
2024-11-19 13:52:41 +0100

    ESlint style

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,28 @@
-import redisClient from '../utils/redis.mjs';
-import dbClient from '../utils/db.mjs';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
-    static getStatus(req, res) {
-        const status = {
-            redis: redisClient.isAlive(),
-            db: dbClient.isAlive(),
-        };
-        res.status(200).json(status);
-    }
+  static getStatus(request, response) {
+    try {
+      const redis = redisClient.isAlive();
+      const db = dbClient.isAlive();
 
-    static async getStats(req, res) {
-        const stats = {
-            users: await dbClient.nbUsers(),
-            files: await dbClient.nbFiles(),
-        };
-        res.status(200).json(stats);
+      response.status(200).send({ redis, db });
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  static async getStats(request, response) {
+    try {
+      const users = await dbClient.nbUsers();
+      const files = await dbClient.nbFiles();
+
+      response.status(200).send({ users, files });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
-export default AppController;
+module.exports = AppController;
