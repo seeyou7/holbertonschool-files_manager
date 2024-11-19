94f4891e86853edacdc21e0da2604f6039b02940
seeyou7 <5764@holbertonstudents.com>
2024-11-19 12:59:22 +0100

    eslint fix

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,17 +1,8 @@
-/**
- * Redis Client Module
- *
- * This module provides a simple wrapper around the Redis client, offering
- * methods to interact with a Redis database. It includes functionality to
- * check connection status, get, set, and delete keys with asynchronous support.
- */
-
-import redis from ('redis');
+import redis from 'redis';
 
 class RedisClient {
     constructor() {
-        this.client = redis.createClient(); 
-        // Connect to Redis
+        this.client = redis.createClient(); // Initialize Redis client
 
         // Handle Redis client errors
         this.client.on('error', (err) => {
@@ -19,20 +10,10 @@ class RedisClient {
         });
     }
 
-    /**
-   * Check if the Redis client is connected
-   * @returns {boolean} True if the client is connected, false otherwise
-   */
     isAlive() {
-        return this.client.connected;
-        // Check if the client is connected
+        return this.client.connected; // Return true if Redis client is connected
     }
 
-      /**
-   * Get the value of a key from Redis
-   * @param {string} key - The key to retrieve from Redis
-   * @returns {Promise<string|null>} A promise that resolves to the value of the key, or null if not found
-   */
     async get(key) {
         return new Promise((resolve, reject) => {
             this.client.get(key, (err, value) => {
@@ -46,13 +27,6 @@ class RedisClient {
         });
     }
 
-    /**
-   * Set a key-value pair in Redis with an expiration time
-   * @param {string} key - The key to set in Redis
-   * @param {string} value - The value to set for the key
-   * @param {number} duration - The expiration time in seconds
-   * @returns {Promise<string>} A promise that resolves to the Redis response
-   */
     async set(key, value, duration) {
         return new Promise((resolve, reject) => {
             this.client.set(key, value, 'EX', duration, (err) => {
@@ -66,11 +40,6 @@ class RedisClient {
         });
     }
 
-    /**
-   * Delete a key from Redis
-   * @param {string} key - The key to delete from Redis
-   * @returns {Promise<number>} A promise that resolves to the number of keys removed
-   */
     async del(key) {
         return new Promise((resolve, reject) => {
             this.client.del(key, (err) => {
